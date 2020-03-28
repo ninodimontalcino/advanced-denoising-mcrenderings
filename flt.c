ab20d1458709193bc59691deb87da642b9284777
Alexandre Poirrier <alexandre.poirrier@polytechnique.org>
2020-03-28 17:21:18 +0100

    FLT: feature weights

--- flt.c
+++ flt.c
@@ -3,8 +3,13 @@
 #include "flt.h"
 
 void flt(buffer *out, buffer *d_out_d_in, buffer *input, buffer *u, buffer *var_u, buffer *f, buffer *var_f, Flt_parameters p) {
-    double wc;
+    double wc, wf;
     double sum_weights;
+
+    buffer *gradients[NB_FEATURES][IMG_SIZE][IMG_SIZE];
+    for(int i=0; i<NB_FEATURES;++i) {
+        compute_gradient(gradients[i], f[i]);
+    }
 
     sum_weights = 0;
     for(int xp=0;xp<IMG_SIZE;++xp) {
@@ -12,6 +17,7 @@ void flt(buffer *out, buffer *d_out_d_in, buffer *input, buffer *u, buffer *var_
             for(int xq = MIN(xp-p.r, 0); xq <= MAX(xp+p.r, IMG_SIZE-1); xq++) {
                 for(int yq = MIN(yp-p.r, 0); yq <= MAX(yp+p.r, IMG_SIZE-1); yq++) {
                     wc = color_weight(u, var_u, p, xp, yp, xq, yq);
+                    wf = feature_weight(f, var_f, gradients, p, xp, yp, xq, yq);
                     // TODO complete
                 }
             }
@@ -43,4 +49,33 @@ double per_pixel_distance(buffer u, buffer var_u, double kc, int xp, int yp, int
     double var_cancel = var_u[xp][yp] + fmin(var_u[xp][yp], var_u[xq][yq]);
     double normalization = EPSILON + kc*kc*(var_u[xp][yp] + var_u[xq][yq]);
     return (sqdist - var_cancel) / normalization;
+}
+
+void compute_gradient(buffer gradient, buffer u) {
+    for(int x=0; x<IMG_SIZE; ++x) {
+        for(int y=0; y<IMG_SIZE; ++y) {
+            //TODO check bounds
+            double diffL = u[x][y] - u[x-1][y];
+            double diffR = u[x][y] - u[x+1][y];
+            double diffU = u[x][y] - u[x][y-1];
+            double diffD = u[x][y] - u[x][y+1];
+
+            gradient[x][y] = fmin(diffL*diffL, diffR*diffR) + fmin(diffU*diffU, diffD*diffD);
+        }
+    }
+}
+
+double feature_weight(buffer *f, buffer *var_f, buffer *gradients, Flt_parameters p, int xp, int yp, int xq, int yq) {
+    double df = 0;
+    for(int j=0; j<NB_FEATURES;++j)
+        df = fmax(df, feature_distance(f[j], var_f[j], gradients[j], p, xp, yp, xq, yq));
+    return exp(-df);
+}
+
+double feature_distance(buffer f, buffer var_f, buffer gradient, Flt_parameters p, int xp, int yp, int xq, int yq) {
+    double sqdist = f[xp][yp] - f[xq][yq];
+    sqdist *= sqdist;
+    double var_cancel = var_f[xp][yp] + fmin(var_f[xp][yp], var_f[xq][yq]);
+    double normalization = p.kf*p.kf*fmax(p.tau, fmax(var_f[xp][yp], gradient[xp][yp]));
+    return (sqdist - var_cancel)/normalization;
 }
--- flt.h
+++ flt.h
@@ -3,12 +3,14 @@
 
 #define IMG_SIZE 512
 #define EPSILON 0.01
+#define NB_FEATURES 3
 
 typedef double** buffer;
 typedef struct
 {
     double kc; //Sensitivity to color
     double kf; //Sensitivity to features
+    double tau; //Threshold
     int f; //Patch radius
     int r; //Window radius
 } Flt_parameters;
@@ -35,7 +37,11 @@ void flt(buffer *out, buffer *d_out_d_in, buffer *input, buffer *u, buffer *var_
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 double per_pixel_distance(buffer u, buffer var_u, double kc, int xp, int yp, int xq, int yq);
-double nl_means_weights(buffer u, buffer var_u, Flt_parameters p, int xp, int yp, int xq, int yq);
+double nl_means_weights(buffer *u, buffer *var_u, Flt_parameters p, int xp, int yp, int xq, int yq);
 double color_weight(buffer *u, buffer *var_u, Flt_parameters p, int xp, int yp, int xq, int yq);
+
+void compute_gradient(buffer gradient, buffer u);
+double feature_distance(buffer f, buffer var_f, buffer gradient, Flt_parameters p, int xp, int yp, int xq, int yq);
+double feature_weight(buffer *f, buffer *var_f, buffer *gradients, Flt_parameters p, int xp, int yp, int xq, int yq);
 
 #endif //FLT_H
