0aed74640afbcebfa35d17c78923cf86a1f0b75c
Burrakauchy <alexandre.binninger@gmail.com>
2020-05-07 13:40:25 +0200

    mend

---
+++ flt_blocking.hpp
@@ -0,0 +1,12 @@
+#ifndef FLT_2_H
+#define FLT_2_H
+
+#include "flt.hpp"
+
+
+void sure_all_blocking(buffer sure, buffer c, buffer c_var, buffer cand_r, buffer cand_g, buffer cand_b, int img_width, int img_height);
+void filtering_basic_blocking(buffer output, buffer input, buffer c, buffer c_var, Flt_parameters p, int img_width, int img_height);
+void feature_prefiltering_blocking(buffer output, buffer output_var, buffer features, buffer features_var, Flt_parameters p, int img_width, int img_height);
+void candidate_filtering_blocking(buffer output, buffer color, buffer color_var, buffer features, buffer features_var, Flt_parameters p, int img_width, int img_height);
+void candidate_filtering_all_blocking(buffer output_r, buffer output_g, buffer output_b, buffer color, buffer color_var, buffer features, buffer features_var, Flt_parameters* p, int img_width, int img_height);
+#endif //FLT_2_H
--- implementations/register.hpp
+++ implementations/register.hpp
@@ -6,6 +6,7 @@
 void basic_implementation(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
 void basic_restructure(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
 void basic_restructure2(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
+void blocking_restructure2(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
 
 /* -------------------------------------------------------------------------
  * FUNCTION REGISTRATION
@@ -15,6 +16,7 @@ void register_functions()
     add_function(&basic_implementation, "Basic Implementation", 1);
     add_function(&basic_restructure, "Restructure (no precomputation)", 1);
     add_function(&basic_restructure2, "Restructure - With some precomputations (prefiltering, candidates)", 1);
+    add_function(&blocking_restructure2, "Blocking with restructure", 1);
 }
 
 #endif
