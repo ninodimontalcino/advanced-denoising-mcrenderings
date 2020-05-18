6b8592ee38f9d356771ab261028ede8c35d93cc6
Burrakauchy <alexandre.binninger@gmail.com>
2020-05-18 21:48:22 +0200

    Creation of basic_restructure4 for Vectorization + vectorization of the sure function.

--- implementations/register.hpp
+++ implementations/register.hpp
@@ -8,6 +8,7 @@ void basic_restructure(buffer out_img, buffer c, buffer c_var, buffer f, buffer 
 void basic_restructure1(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
 void basic_restructure2(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
 void basic_restructure3(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
+void basic_restructure4(buffer out_img, buffer c, buffer c_var, buffer f, buffer f_var, int R, int img_width, int img_height);
 
 /* -------------------------------------------------------------------------
  * FUNCTION REGISTRATION
@@ -19,6 +20,7 @@ void register_functions()
     add_function(&basic_restructure1, "Restructure + ILP", 1);
     add_function(&basic_restructure2, "Restructure + Precomputations", 1);
     add_function(&basic_restructure3, "Restructure + Precomputations + ILP", 1);
+    add_function(&basic_restructure4, "Restructure + Precomputations + ILP + Vectorization", 1);
 }
 
 #endif
